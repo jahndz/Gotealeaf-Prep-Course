bc4d533647fd145d028c4f95b21d509a81edbdb8
jahndz <jaguilarhndz@gmail.com>
2015-06-24 21:49:46 -0700

    Added the variables file with exercises

---
+++ Variables/exercise_1.rb
@@ -0,0 +1,3 @@
+puts "Hi there! What's your name?"
+name = gets.chomp.capitalize
+puts "Nice to meet you #{name}." 
---
+++ Variables/exercise_4.rb
@@ -0,0 +1,5 @@
+puts "What's your first name?"
+first_name = gets.chomp.capitalize
+puts "What's your last name?"
+last_name = gets.chomp.capitalize
+puts "Hello #{first_name} #{last_name}!"
